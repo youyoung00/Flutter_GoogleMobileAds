d0ce3da70f78eb80082ff8b3a2bd71f99e1d1181
youyoung00 <you889@hanmail.net>
2022-02-23 00:04:29 +0900

    구글 애드몹 광고 붙이기 연습 앱

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,12 @@ class _MyAppState extends State<MyApp> {
   late BannerAd inlineAd;
   bool inlineAdLoaded = false;
 
+  InterstitialAd? interstitialAd;
+  int interstitialAttempts = 0;
+
+  RewardedAd? rewardedAd;
+  int rewardedAdAttempts = 0;
+
   static const AdRequest request = AdRequest(
       // keywords: ['', ''],
       // contentUrl: '',
@@ -52,7 +58,7 @@ class _MyAppState extends State<MyApp> {
   }
 
   void loadInlineBannerAd() {
-    staticAd = BannerAd(
+    inlineAd = BannerAd(
       adUnitId: BannerAd.testAdUnitId,
       size: AdSize.banner,
       request: request,
@@ -69,13 +75,109 @@ class _MyAppState extends State<MyApp> {
       ),
     );
 
-    staticAd.load();
+    inlineAd.load();
+  }
+
+  void createInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: InterstitialAd.testAdUnitId,
+      request: request,
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          interstitialAd = ad;
+          interstitialAttempts = 0;
+        },
+        onAdFailedToLoad: (error) {
+          interstitialAttempts++;
+          interstitialAd = null;
+          print('falied to load ${error.message}');
+
+          if (interstitialAttempts <= maxAttempt) {
+            createInterstitialAd();
+          }
+        },
+      ),
+    );
+  }
+
+  void showInterstitialAd() {
+    if (interstitialAd == null) {
+      print('trying to show before loading');
+      return;
+    }
+
+    interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (ad) => print('ad showed $ad'),
+      onAdDismissedFullScreenContent: (ad) {
+        ad.dispose();
+        createInterstitialAd();
+      },
+      onAdFailedToShowFullScreenContent: (ad, error) {
+        ad.dispose();
+        print('failed to show the ad $ad');
+
+        createInterstitialAd();
+      },
+    );
+
+    interstitialAd!.show();
+    interstitialAd = null;
+  }
+
+  void createRewardedAd() {
+    RewardedAd.load(
+      adUnitId: RewardedAd.testAdUnitId,
+      request: request,
+      rewardedAdLoadCallback: RewardedAdLoadCallback(
+        onAdLoaded: (ad) {
+          rewardedAd = ad;
+          rewardedAdAttempts = 0;
+        },
+        onAdFailedToLoad: (error) {
+          rewardedAdAttempts++;
+          rewardedAd = null;
+          print('failed to load ${error.message}');
+
+          if (rewardedAdAttempts <= maxAttempt) {
+            createRewardedAd();
+          }
+        },
+      ),
+    );
+  }
+
+  void showRewardedAd() {
+    if (rewardedAd == null) {
+      print('trying to show before loading');
+      return;
+    }
+
+    rewardedAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (ad) => print('ad showed $ad'),
+      onAdDismissedFullScreenContent: (ad) {
+        ad.dispose();
+        createRewardedAd();
+      },
+      onAdFailedToShowFullScreenContent: (ad, error) {
+        ad.dispose();
+        print('failed to show the ad $ad');
+
+        createRewardedAd();
+      },
+    );
+
+    rewardedAd!.show(onUserEarnedReward: (ad, reward) {
+      print('reward video ${reward.amount} ${reward.type}');
+    });
+    rewardedAd = null;
   }
 
   @override
   void initState() {
     loadInlineBannerAd();
     loadStaticBannerAd();
+    createInterstitialAd();
+    createRewardedAd();
     super.initState();
   }
 
@@ -95,66 +197,70 @@ class _MyAppState extends State<MyApp> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: SingleChildScrollView(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              if (staticAdLoaded)
-                Container(
-                  child: AdWidget(ad: staticAd),
-                  width: staticAd.size.width.toDouble(),
-                  height: staticAd.size.height.toDouble(),
-                  alignment: Alignment.bottomCenter,
-                ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Show Interstitial Ad'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (staticAdLoaded)
+                  Container(
+                    child: AdWidget(ad: staticAd),
+                    width: staticAd.size.width.toDouble(),
+                    height: staticAd.size.height.toDouble(),
+                    alignment: Alignment.bottomCenter,
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Show Rewarded Ad'),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height - 300,
-                child: ListView.builder(
-                  itemCount: 20,
-                  itemBuilder: (context, index) {
-                    if (inlineAdLoaded && index == 5) {
-                      return Column(
-                        children: [
-                          Container(
-                            child: AdWidget(ad: inlineAd),
-                            width: inlineAd.size.width.toDouble(),
-                            height: inlineAd.size.height.toDouble(),
-                          ),
-                          const SizedBox(height: 20),
-                          ListTile(
-                            title: Text('Item ${index + 1}'),
-                            leading: const Icon(Icons.star),
-                          ),
-                        ],
-                      );
-                    } else {
-                      return ListTile(
-                        title: Text('Item ${index + 1}'),
-                        leading: const Icon(Icons.star),
-                      );
-                    }
-                  },
+                const SizedBox(
+                  height: 30,
                 ),
-              )
-            ],
-          )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        showInterstitialAd();
+                      },
+                      child: const Text('Show Interstitial Ad'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        showRewardedAd();
+                      },
+                      child: const Text('Show Rewarded Ad'),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height - 300,
+                  child: ListView.builder(
+                    itemCount: 20,
+                    itemBuilder: (context, index) {
+                      if (inlineAdLoaded && index == 5) {
+                        return Column(
+                          children: [
+                            SizedBox(
+                              child: AdWidget(ad: inlineAd),
+                              width: inlineAd.size.width.toDouble(),
+                              height: inlineAd.size.height.toDouble(),
+                            ),
+                            ListTile(
+                              title: Text('Item ${index + 1}'),
+                              leading: const Icon(Icons.star),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return ListTile(
+                          title: Text('Item ${index + 1}'),
+                          leading: const Icon(Icons.star),
+                        );
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
